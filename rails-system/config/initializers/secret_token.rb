5d7b35eb5b31bbced99226bc9b44d621f5730dfd
Atsushi Nakatsugawa <atsushi@moongift.jp>
2011-01-18 09:30:38 +0900

    WordPress and Rails base system

---
+++ rails-system/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsSystem::Application.config.secret_token = '9bc6b1c3b2077ad7c4db8d8627a81012c4ce2b5ca3cc8fe7ae0fc650b9205ac3d88ff1fbbc1caa080f4b7bb28ee8249430254ecf57e6f755d10654565811bc71'
